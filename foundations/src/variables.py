b52eba1e5f9cb7f144956088941eb5ea8063e8f8
nchandolaj <nchandolaj@gmail.com>
2025-12-22 09:12:32 +0000

    updated

--- foundations/src/variables.py
+++ foundations/src/variables.py
@@ -13,3 +13,19 @@ print("\n*** Local and Global Variables ***")
 
 print("\nLocal variables can only be used in a method or classes that your specify.")
 print("\nGlobal variables can be used in any part of the program without issue.")
+
+# Global Variables
+x = 6
+print((f"After initalization: {x=}"))
+
+def my_def(x):
+  x = 8
+  print((f"Inside function: {x=}"))  
+
+  global y
+  y = "hello"
+
+my_def(x)
+print((f"After making function call: {x=}"))
+
+print(f"Calling global variable declared inside function: {y=}")
