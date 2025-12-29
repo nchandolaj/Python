09aebf08262f1da14cbbf99065bfa5fc3c90094e
nchandolaj <nchandolaj@gmail.com>
2025-12-29 09:43:35 +0000

    updated with a second function

--- foundations/src/functions.py
+++ foundations/src/functions.py
@@ -2,6 +2,7 @@
 Python Functions
 '''
 
+# First functions
 def calculate_area(length, width):
   return length * width
 
@@ -9,3 +10,9 @@ length, width = 10, 30
 area = calculate_area(length, width)
 print(f"For {length=} and {width=}, {area=}")
 
+# Second function
+def celsius_to_fahrenheit(c):
+  return (c * 9/5) + 32
+
+c = 5
+print (f"Celsius {c} is that same as {celsius_to_fahrenheit(c)} Fahrenheit.")
