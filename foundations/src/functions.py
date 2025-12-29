533cefc8c60d0b3eccc766afc040a8b517b6bb91
nchandolaj <nchandolaj@gmail.com>
2025-12-29 09:39:30 +0000

    initial file

---
+++ foundations/src/functions.py
@@ -0,0 +1,11 @@
+'''
+Python Functions
+'''
+
+def calculate_area(length, width):
+  return length * width
+
+length, width = 10, 30
+area = calculate_area(length, width)
+print(f"For {length=} and {width=}, {area=}")
+
